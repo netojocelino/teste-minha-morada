4da850eee2e2f95565135f1dac8db228f9185d0f
Neto Jocelino <netoj96@live.com>
2023-02-07 19:16:35 -0300

    reset password initial solution

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,10 @@ use App\Models as Model;
 use App\Services\CepapiService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+use Illuminate\Auth\Events\PasswordReset;
 
 /*
 |--------------------------------------------------------------------------
@@ -97,3 +100,52 @@ Route::post('/user', function (Request $request) {
     return response()
         ->json($body);
 });
+
+Route::post('/forgot-password', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+    ]);
+
+    $status = Password::sendResetLink(
+        $request->only('email')
+    );
+
+    return response()
+        ->json([
+            'email' => $request->email,
+            'status' => $status,
+        ]);
+})->name('password.email');
+
+
+
+Route::post('/reset-password', function (Request $request) {
+    try {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return response()
+            ->json([
+                'status' => $status,
+                'email' => $request->email,
+            ]);
+    } catch (\Exception $exception) {
+        return response($exception->getMessage(), 500);
+    }
+})->name('password.update');
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/forgot-password', function () {
+    return [
+        'page' => 'forgot-password'
+    ];
+    return view('auth.forgot-password');
+})->middleware('guest')->name('password.request');
+
+Route::get('/reset-password/{token}', function (string $token) {
+    return [
+        'token' => $token,
+    ];
+})->name('password.reset');
