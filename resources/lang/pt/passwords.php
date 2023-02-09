5f9bb01cb807449323f455777102ede6073cdc5c
Neto Jocelino <netoj96@live.com>
2023-02-08 22:38:43 -0300

    add language

---
+++ resources/lang/pt/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Sua senha foi resetada!',
+    'sent' => 'Nós te enviamos um link para recuperar senha!',
+    'throttled' => 'Por favor, tente novamente mais tarde.',
+    'token' => 'Token inválido.',
+    'user' => "Nós não conseguimos encontrar este endereço de email.",
+
+];
